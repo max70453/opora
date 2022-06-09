1e3f0b9db9afc8bd3e77eaeb89784c348846fb4f
munja7 <munja7@mail.ru>
2022-06-09 12:49:10 +0300

    develop| close burger menu on links click

--- src/blocks/header-mobile/index.js
+++ src/blocks/header-mobile/index.js
@@ -1,1 +1,13 @@
 import './header-mobile.sass';
+
+var root = $('.mobile-menu');
+
+var links = root.find("a");
+
+links.on('click', function(){
+    $('.header-mobile__menu').removeClass('open');
+    $(".burger").removeClass('is-active');
+    $('body').removeClass('stop-scroll');
+})
+
+
